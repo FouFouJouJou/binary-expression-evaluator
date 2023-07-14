53a745bdd117af8db65e8f27a1f6d813c927745f
firasjaballiftw <firasjaballiftw@gmail.com>
2023-07-14 08:38:25 +0100

    chaning exit codes

--- source/main.c
+++ source/main.c
@@ -15,7 +15,7 @@ int16_t evaluate(char *expression_string) {
 int main(int argc, char **argv) {
   if(argc != 2 || *argv[1]=='\0') {
     fprintf(stderr, "No expression provided: expr argument is (nil)\n");
-    exit(420);
+    exit(690);
   }
   char *expression=argv[1];
   printf("evaluating: %s\n", expression);
